39966ad750440ac683012c40e81a901763df4a02
luodanwg <luodan.wg@gmail.com>
2017-03-17 23:40:43 +0800

    init

--- View/Invoke.go
+++ View/Invoke.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"GoOnchain/common/log"
 )
 
 const (
@@ -87,24 +88,27 @@ func (t *SimpleChaincode) InvokeGetData(stub shim.ChaincodeStubInterface, args [
 	var CHandler = NewCertHandler()
 	sigma, err := stub.GetCallerMetadata()
 	if err != nil {
+		log.Info("Failed getting metadata")
 		return nil, errors.New("Failed getting metadata")
 	}
 	payload, err := stub.GetPayload()
 	if err != nil {
+		log.Info("Failed getting payload")
 		return nil, errors.New("Failed getting payload")
 	}
 	binding, err := stub.GetBinding()
 	if err != nil {
+		log.Info("Failed getting binding")
 		return nil, errors.New("Failed getting binding")
 	}
 
-	Logger.Debugf("passed sigma [% x]", sigma)
-	Logger.Debugf("passed payload [% x]", payload)
-	Logger.Debugf("passed binding [% x]", binding)
+	Logger.Info("passed sigma [% x]", sigma)
+	Logger.Info("passed payload [% x]", payload)
+	Logger.Info("passed binding [% x]", binding)
 
 	isAuthorized, err := CHandler.IsAuthorized(stub, "client")
 	if !isAuthorized {
-		Logger.Errorf("system error %v", err)
+		Logger.Info("system error %v", err)
 		return nil, errors.New("user is not aurthorized to assign assets")
 	}
 
